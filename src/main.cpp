6959d71fa775e8a03bbf41a9e4b1c9e9232d6869
Wagner <wagnermc506@gmail.com>
2019-04-17 17:16:10 -0300

    posicoes setadas na main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
- #include <iostream>
+#include <iostream>
 #include <string>
 #include <fstream>
-#include "../inc/mapa.hpp"
+//#include "../inc/mapa.hpp"
 
 using namespace std;
 
 int main(){
 
-    Mapa mapa1;
+/*    Mapa mapa1;
 
+    //Desenhar Mapa
     string lineMain[13], lineSub;
     lineMain[0] = mapa1.create_lineMain();
     lineSub = mapa1.create_lineSub();
@@ -16,6 +17,21 @@ int main(){
         lineMain[i] = lineMain[0];
     }
     mapa1.show_map(lineMain, lineSub);
-
+*/
+    //Setar posições
+    int position[13][13];
+    for (int i = 0; i < 13; i++){
+        for(int j = 0; j < 13; j++){
+            position[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < 13; i++){
+        for(int j = 0; j < 13; j++){
+            cout << position[i][j] << " ";
+            if(j == 12){
+                cout << endl;
+            }
+        }
+    }
     return 0;
 }
